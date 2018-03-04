fc81c85e23423ec258123215da67269107fd67a8
raimundoGarcia <raigarva@gmail.com>
2018-03-04 03:33:08 +0100

    cambio peso fotos

--- js/galeria.js
+++ js/galeria.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
         var fotos = "<div id='fotos-galeria'>";
 
 
-        fotos += '   <div id="' + '20170717_155914.jpg' + '" class="card">  ' +
-                ' <img src="./resources/images/galeria/' + '20170717_155914.jpg' + '" class="imagen-galeria" alt="">  ' +
+        fotos += '   <div id="' + '0004.jpg' + '" class="card">  ' +
+                ' <img src="./resources/images/galeria/' + '0004.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
         fotos += '   <div id="' + '20180225_184137.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '20180225_184137.jpg' + '" class="imagen-galeria" alt="">  ' +
@@ -15,14 +15,14 @@ $(document).ready(function () {
         fotos += '   <div id="' + '20180225_184614.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '20180225_184614.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
-        fotos += '   <div id="' + 'ANA-Y-LUIS-90.jpg' + '" class="card">  ' +
-                ' <img src="./resources/images/galeria/' + 'ANA-Y-LUIS-90.jpg' + '" class="imagen-galeria" alt="">  ' +
+        fotos += '   <div id="' + '0001.jpg' + '" class="card">  ' +
+                ' <img src="./resources/images/galeria/' + '0001.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
-        fotos += '   <div id="' + 'MG_9918.jpg' + '" class="card">  ' +
-                ' <img src="./resources/images/galeria/' + 'MG_9918.jpg' + '" class="imagen-galeria" alt="">  ' +
+        fotos += '   <div id="' + '0002' + '" class="card">  ' +
+                ' <img src="./resources/images/galeria/' + '0002.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
-        fotos += '   <div id="' + 'novios_boda.jpg' + '" class="card">  ' +
-                ' <img src="./resources/images/galeria/' + 'novios_boda.jpg' + '" class="imagen-galeria" alt="">  ' +
+        fotos += '   <div id="' + '0003.jpg' + '" class="card">  ' +
+                ' <img src="./resources/images/galeria/' + '0003.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
 
 
@@ -33,7 +33,7 @@ $(document).ready(function () {
         $("#contenedor").html("");
         $("#contenedor").append(fotos);
         $("#fotos-galeria").on("click", ".card", function () {
-            alert($(this).attr("id" ));
+            alert($(this).attr("id"));
 
         });
 

61f60e35868a7e215344e6c2b07f53afb2a31260
raimundoGarcia <raigarva@gmail.com>
2018-03-04 04:31:44 +0100

    modales 1.0

--- js/galeria.js
+++ js/galeria.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $("#btn-galeria").on("click", function () {
         var fotos = "<div id='fotos-galeria'>";
-
-
         fotos += '   <div id="' + '0004.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '0004.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
@@ -18,23 +16,38 @@ $(document).ready(function () {
         fotos += '   <div id="' + '0001.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '0001.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
-        fotos += '   <div id="' + '0002' + '" class="card">  ' +
+        fotos += '   <div id="' + '0002.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '0002.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
         fotos += '   <div id="' + '0003.jpg' + '" class="card">  ' +
                 ' <img src="./resources/images/galeria/' + '0003.jpg' + '" class="imagen-galeria" alt="">  ' +
                 '  </div>';
-
-
-
-
-
         fotos += '</div>';
+        fotos +=  '<!-- Modal -->' +
+                    '<div id="myModal" class="modal fade" role="dialog">' +
+                    '  <div class="modal-dialog modal-lg">' +
+                    '     <!-- Modal content-->' +
+                    '    <div class="modal-content">' +
+                    '       <div class="modal-header">' +
+                    '          <button type="button" class="close" data-dismiss="modal">&times;</button>' +
+                    
+                    '    </div>' +
+                    '   <div class="modal-body">' +
+                    ' <img id="ModalImage" src="./resources/images/galeria/' + '0001.jpg' + '" class="imagen-galeria" alt="">  ' +
+                    ' </div>' +
+                    ' <div class="modal-footer">' +
+                    '    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+                    ' </div>' +
+                    ' </div>' +
+                    '</div>' +
+                    '</div>';
         $("#contenedor").html("");
         $("#contenedor").append(fotos);
         $("#fotos-galeria").on("click", ".card", function () {
-            alert($(this).attr("id"));
-
+            var modalID =$(this).attr("id");
+            var modalImg = './resources/images/galeria/' + modalID ;
+            $("#ModalImage").attr("src",modalImg);
+            $("#myModal").modal("show");
         });
 
     });

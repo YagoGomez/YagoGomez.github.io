eb70030915854914e3d93f9fca0fb24214ef5684
raimundoGarcia <raigarva@gmail.com>
2018-03-05 17:36:07 +0100

    galeria con modales 1.0

--- js/galeria.js
+++ js/galeria.js
@@ -24,21 +24,16 @@ $(document).ready(function () {
                 '  </div>';
         fotos += '</div>';
         fotos +=  '<!-- Modal -->' +
-                    '<div id="myModal" class="modal fade" role="dialog">' +
-                    '  <div class="modal-dialog modal-lg">' +
+                    '<div id="myModal" class="modal fullscreen-modal fade" role="dialog">' +
+                    '  <div class="modal-dialog">' +
                     '     <!-- Modal content-->' +
                     '    <div class="modal-content">' +
-                    '       <div class="modal-header">' +
-                    '          <button type="button" class="close" data-dismiss="modal">&times;</button>' +
                     
-                    '    </div>' +
-                    '   <div class="modal-body">' +
-                    ' <img id="ModalImage" src="./resources/images/galeria/' + '0001.jpg' + '" class="imagen-galeria" alt="">  ' +
+                    '          <button type="button" class="close" data-dismiss="modal">&times;</button>' +
                     ' </div>' +
-                    ' <div class="modal-footer">' +
-                    '    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
-                    ' </div>' +
-                    ' </div>' +
+                    '<div class="img-modal" >'+
+                    ' <img id="ModalImage" src="./resources/images/galeria/' + '0003.jpg' + '" class="imagen-modal" alt="">  ' +
+                    '</div>'+
                     '</div>' +
                     '</div>';
         $("#contenedor").html("");
@@ -49,9 +44,28 @@ $(document).ready(function () {
             $("#ModalImage").attr("src",modalImg);
             $("#myModal").modal("show");
         });
+//        
+//        var fotos = '<div class="galleria">' +
+//                '<img src="resources/images/galeria/0001.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0002.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0003.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0004.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0005.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0006.jpg" alt=""/>' +
+//                '<img src="resources/images/galeria/0007.jpg" alt=""/>' +
+//                '</div>';
+//        $("#contenedor").html("");
+//        $("#contenedor").append(fotos);
+//
+//        (function () {
+//            Galleria.loadTheme('vendor/galleria/themes/classic/galleria.classic.min.js');
+//            Galleria.configure({
+//                carousel: false
+//            });
+//            Galleria.run('.galleria');
 
+//        }());
     });
-
 });
 
 
